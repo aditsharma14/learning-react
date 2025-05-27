249eaf766a68a706ea808c7dc49f6f024dc1fad1
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-05-27 13:31:01 +0530

    practiced rendering multiple components

---
+++ fundamentals of react/multiple components/part-2/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./assets/components/Component for react/header";
+import MainContent from "./assets/components/Component for react/main content";
+import Footer from "./assets/components/Component for react/footer";
+const App=()=>{
+  return(
+    <>
+      <Header/>
+      <MainContent/>
+      <Footer/>
+    </>
+
+  )
+}
+export default App;
---
+++ fundamentals of react/multiple components/part-2/main content.jsx
@@ -0,0 +1,9 @@
+const MainContent=()=>{
+    return(
+        <main>
+            <h2>Main Content</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis non in sit sint modi tempore distinctio omnis aut adipisci quia! Temporibus, voluptate maiores. Fugit iure ea aspernatur eos pariatur? Id.</p>
+        </main>
+    )
+}
+export default MainContent;
