2cde035890b1da551999331b860ad8987c89f5cf
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-05-27 12:50:26 +0530

    rendering multiple components

---
+++ fundamentals of react/multiple components/part-1/App.jsx
@@ -0,0 +1,11 @@
+import Add from "./assets/components/Component for react/add";
+import Greeting from "./assets/components/Component for react/greeting";
+const App=()=>{
+  return(
+    <>
+    <Add/>
+    <Greeting/>
+    </>
+  )
+};
+export default App;
