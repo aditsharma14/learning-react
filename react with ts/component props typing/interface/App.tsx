99917204ee8a251d1aada87273ea63e9ee15222d
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-06-02 15:30:08 +0530

    using type alises or interface

---
+++ react with ts/component props typing/interface/App.tsx
@@ -0,0 +1,10 @@
+import User from './assets/props/first';
+
+const App = () => {
+  return (
+    <section>
+      <User name={'adit'} age={18} isstudent={false}/>
+    </section>
+  )
+}
+export default App
---
+++ react with ts/component props typing/interface/first.tsx
@@ -0,0 +1,16 @@
+//using type alises or interface
+interface UserShape{
+    name:string;
+    age:number;
+    isstudent:boolean;
+}
+const User=({name,age,isstudent}:UserShape)=>{
+    return(
+        <main>
+            <h1>Name:{name}</h1>
+            <p>Age:{age}</p>
+            <p>Student:{isstudent.toString()}</p>
+        </main>
+    );
+};
+export default User;
